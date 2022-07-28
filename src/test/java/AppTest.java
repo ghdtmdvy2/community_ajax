1f924daf035d8ebf9b30de6bedaca1b40a94b0dd
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-28 10:23:52 +0900

    json 테스트

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,3 +1,7 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.member.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,4 +12,15 @@ public class AppTest {
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
     }
+    @Test
+    void 실험_json() throws JsonProcessingException {
+        ArticleDto articleDto = new ArticleDto(1, "제목1","내용1");
+        String rs = Ut.json.toJsonStr(articleDto,"");
+        assertThat(rs).isNotBlank();
+        assertThat(rs).isEqualTo("""
+                {"id":1,"title":"제목1","body":"내용1"}
+                """.trim());
+    }
+
+
 }
