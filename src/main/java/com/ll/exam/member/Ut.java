731fe57a0fbaa5aa9bd2bd62a81daf1a763d5e30
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-07-28 11:50:40 +0900

    json을 객체화

--- src/main/java/com/ll/exam/member/Ut.java
+++ src/main/java/com/ll/exam/member/Ut.java
@@ -2,16 +2,29 @@ package com.ll.exam.member;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
 
 public class Ut {
     public static class json {
+        private static ObjectMapper om;
+        static{
+             om = new ObjectMapper();
+        }
         public static String toStr(Object obj, String defaultValue) {
-            ObjectMapper om = new ObjectMapper();
 
             try {
                 return om.writeValueAsString(obj);
             } catch (JsonProcessingException e) {
                 return defaultValue;
+            }
+        }
+
+
+        public static <T> T toObj(String jsonStr, Class<T> cls, Object o) {
+            try {
+                return om.readValue(jsonStr, cls);
+            } catch (JsonProcessingException e) {
+                return null;
             }
         }
     }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -13,13 +13,21 @@ public class AppTest {
         assertThat(rs).isEqualTo(30);
     }
     @Test
-    void 실험_객체에서_json() throws JsonProcessingException {
+    void ObjectMapper_객체에서_json() throws JsonProcessingException {
         ArticleDto articleDto = new ArticleDto(1, "제목1","내용1");
         String rs = Ut.json.toStr(articleDto,"");
         assertThat(rs).isNotBlank();
         assertThat(rs).isEqualTo("""
                 {"id":1,"title":"제목1","body":"내용1"}
                 """.trim());
+    }
+
+    @Test
+    void ObjectMapper_json에서_객체() throws JsonProcessingException {
+        ArticleDto articleDtoOrigin = new ArticleDto(1, "제목1","내용1");
+        String jsonStr = Ut.json.toStr(articleDtoOrigin,"");
+        ArticleDto articleDtoObj = Ut.json.toObj(jsonStr,ArticleDto.class,null);
+        assertThat(articleDtoOrigin).isEqualTo(articleDtoObj);
     }
 
 
